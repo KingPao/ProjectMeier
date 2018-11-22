b159f0a39125969e5261cb9ff6c079dc260a2188
JakobGeringer <jakob.geringer@accenture.com>
2018-11-22 10:48:20 +0100

    delete .txt refactor imports

--- src/map/TileMap.java
+++ src/map/TileMap.java
@@ -4,7 +4,6 @@ import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Random;
 import java.util.Scanner;
 
 import org.newdawn.slick.GameContainer;
