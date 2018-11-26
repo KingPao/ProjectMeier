7eff477cdacc74dc32eb505861276b4c014cb5a5
e.hrbatovic <e.hrbatovic@CPX-HU4HPERILHN.dir.svc.accenture.com>
2018-11-26 20:37:07 +0100

    parse collidable matrix from file, assign collidables to map

--- src/map/TileMap.java
+++ src/map/TileMap.java
@@ -2,6 +2,7 @@ package map;
 
 import java.awt.Point;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -18,14 +19,17 @@ public class TileMap extends GameEntity {
 	private Tile[][] tileMap;
 	private Point[][] coordinateMap;
 	private SpriteSheet sprites;
-	private int columns;
-	private int rows;
+	private int columnsMap;
+	private int rowsMap;
+	private int columnsMatrix;
+	private int rowsMatrix;
+	private boolean[][] collideMatrix;
 
 	public TileMap(SpriteSheet sprites) {
 		this.sprites = sprites;
 
-
 		try {
+			getCollidableTextures();
 			readMapFromFile();
 			generateMap();
 		} catch (IOException e) {
@@ -33,42 +37,60 @@ public class TileMap extends GameEntity {
 		}
 	}
 
+	public void getCollidableTextures() throws FileNotFoundException {
+		Scanner sc = new Scanner(new BufferedReader(new FileReader(GameConfig.FILE_DEMOMAP_COLLIDABLE)));
+		String[] matrixSize = sc.nextLine().replace(";", "").split("\\|");
+
+		columnsMatrix = Integer.parseInt(matrixSize[0]);
+		rowsMatrix = Integer.parseInt(matrixSize[1]);
+
+		collideMatrix = new boolean[columnsMatrix][rowsMatrix];
+
+		for (int y = 0; y < rowsMatrix; y++) {
+			String line = sc.nextLine();
+			String[] textures = line.split(";");
+
+			for (int x = 0; x < columnsMatrix; x++) {
+				if (textures[x].equals("1")) {
+					collideMatrix[x][y] = true;
+				}
+
+			}
+		}
+	}
 
 	public void readMapFromFile() throws IOException {
 		Scanner sc = new Scanner(new BufferedReader(new FileReader(GameConfig.FILE_DEMOMAP)));
 		String[] mapsSize = sc.nextLine().replace(";", "").split("\\|");// Delete Excel Generated Delimitors in first
 																		// line
 
-		columns = Integer.parseInt(mapsSize[0]);
-		rows = Integer.parseInt(mapsSize[1]);
+		columnsMap = Integer.parseInt(mapsSize[0]);
+		rowsMap = Integer.parseInt(mapsSize[1]);
 
+		coordinateMap = new Point[columnsMap][rowsMap];
 
-		coordinateMap = new Point[columns][rows];
-
-		for (int y = 0; y < rows; y++) {
+		for (int y = 0; y < rowsMap; y++) {
 			String line = sc.nextLine();
 			String sprites[] = line.split(";");
 
-			for (int x = 0; x < columns; x++) {
+			for (int x = 0; x < columnsMap; x++) {
 				int spriteX = Integer.parseInt(sprites[x].split("\\|")[0]);
 				int spriteY = Integer.parseInt(sprites[x].split("\\|")[1]);
 				coordinateMap[x][y] = new Point(spriteX, spriteY);
-
 			}
 		}
-		
-		
+
 	}
 
 	void generateMap() {
-		tileMap = new Tile[columns][rows];
+		tileMap = new Tile[columnsMap][rowsMap];
 
-		for (int y = 0; y < rows; y++) {
-			for (int x = 0; x < columns; x++) {
+		for (int y = 0; y < rowsMap; y++) {
+			for (int x = 0; x < columnsMap; x++) {
 				int spriteX = coordinateMap[x][y].x;
 				int spriteY = coordinateMap[x][y].y;
 				tileMap[x][y] = new Tile(sprites.getTileTextures()[spriteX][spriteY].getTileTexture(), x, y);
-				if(spriteX == 7 && spriteY == 3) {
+				if (collideMatrix[spriteX][spriteY]) {
 					tileMap[x][y].setCollidable(true);
 				}
 
@@ -83,8 +105,8 @@ public class TileMap extends GameEntity {
 
 		for (int x = 0; x < GameConfig.SCREEN_WIDTH / GameConfig.TILE_SIZE; x++) {
 			for (int y = 0; y < GameConfig.SCREEN_HEIGHT / GameConfig.TILE_SIZE; y++) {
-				tileMap[x][y].getTileTexture().drawEmbedded(x * GameConfig.TILE_SIZE, y * GameConfig.TILE_SIZE, GameConfig.TILE_SIZE,
-						GameConfig.TILE_SIZE);
+				tileMap[x][y].getTileTexture().drawEmbedded(x * GameConfig.TILE_SIZE, y * GameConfig.TILE_SIZE,
+						GameConfig.TILE_SIZE, GameConfig.TILE_SIZE);
 			}
 		}
 		sprites.getSpriteSheet().endUse();
